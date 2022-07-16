60eaa5002f3e886ea28b9f35f6e28822bf0be51e
Vlad <Promotion-kmv@yandex.ru>
2022-07-16 14:57:31 +0300

    first commit

---
+++ get_data/get_data.py
@@ -0,0 +1,33 @@
+import os
+from time import sleep
+
+import requests
+from dotenv import load_dotenv
+import httplib2
+import apiclient.discovery
+from oauth2client.service_account import ServiceAccountCredentials
+from get_data.config import CREDENTIALS_FILE
+from loguru import logger
+
+load_dotenv()
+# Авторизуемся и получаем service — экземпляр доступа к API
+credentials = ServiceAccountCredentials.from_json_keyfile_name(
+    CREDENTIALS_FILE,
+    ['https://www.googleapis.com/auth/spreadsheets',
+     'https://www.googleapis.com/auth/drive'])
+httpAuth = credentials.authorize(httplib2.Http())
+service = apiclient.discovery.build('sheets', 'v4', http = httpAuth)
+
+# Получаем данные из google sheets
+def get_data_sheets():
+    logger.info('Получаю данные из таблицы')
+    values = service.spreadsheets().values().get(
+        spreadsheetId=os.getenv('SPREADSHEET_ID'),
+        range='A1:D51',
+        majorDimension='ROWS'
+    ).execute()
+    return values
+
+# Получаем курс доллара
+url_cb = 'https://www.cbr-xml-daily.ru/daily_json.js'
+get_exchange_rate = lambda: requests.get(url_cb).json()['Valute']['USD']['Value']
